21e31c2ef6b5ba84b6f9f46a8419255c6668a348
Javier <javier.stampa@gmail.com>
2026-08-12 11:51:24 +0200

    App iOS GasAll: wrapper WKWebView + build del .ipa en GitHub Actions

---
+++ ios/GasAll/GasAllApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct GasAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .ignoresSafeArea()
+        }
+    }
+}
---
+++ ios/GasAll/WebView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://javierstampa33.pythonanywhere.com/")!)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeCoordinator() -> Coordinator { Coordinator() }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences.allowsContentJavaScript = true
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.uiDelegate = context.coordinator
+        webView.navigationDelegate = context.coordinator
+        webView.allowsBackForwardNavigationGestures = true
+        webView.scrollView.bounces = false
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+
+    final class Coordinator: NSObject, WKNavigationDelegate, WKUIDelegate {
+        func webView(_ webView: WKWebView,
+                     requestGeolocationPermissionFor origin: WKSecurityOrigin,
+                     initiatedByFrame frame: WKFrameInfo,
+                     decisionHandler: @escaping (WKPermissionDecision) -> Void) {
+            decisionHandler(.prompt)
+        }
+
+        func webView(_ webView: WKWebView,
+                     requestUserMediaPermissionFor origin: WKSecurityOrigin,
+                     initiatedByFrame frame: WKFrameInfo,
+                     type: WKMediaCaptureType,
+                     decisionHandler: @escaping (WKPermissionDecision) -> Void) {
+            decisionHandler(.prompt)
+        }
+    }
+}
